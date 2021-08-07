8d5df0e389879549a1529730b15e8929437233ee
Justin Phillips <Jukphillips1990@gmail.com>
2021-08-06 21:50:38 -0600

    Created invisible input fields and began working on taking those inputs and iterating through them

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,10 @@
+// setting variable for with time and moment format
 var currentDay = moment().format('dddd, MMMM Do')
+// calling that variable to be displayed on html id 'currentDay
 $("#currentDay").text(currentDay)
+var input1Con = document.querySelector('#input1area')
+
+
+
+var i = 1;
+console.log(input+i+Con)
