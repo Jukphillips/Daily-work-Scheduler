e8fab62faeeab230edabdf2b8f222f4717e48aa7
Justin Phillips <Jukphillips1990@gmail.com>
2021-08-08 15:38:57 -0600

    created all inpu values anf local storage

--- Develop/script.js
+++ Develop/script.js
@@ -3,12 +3,17 @@ var currentDay = moment().format('dddd, MMMM Do')
 // calling that variable to be displayed on html id 'currentDay
 $("#currentDay").text(currentDay)
 var input1Con = document.querySelector('#input1area')
+var clear = document.querySelector('#clear')
 
 nineAm();
 tenAm();
 elevenAm();
 twelvePm();
-thirteenAm();
+thirteenPm();
+fourteenPm();
+fifteenPm();
+sixteenPm();
+seventeenPm();
 
 
 
@@ -28,6 +33,7 @@ function nineAm () {
     function storeInfo9() {
         localStorage.setItem("UserInput9", input9area.value);
         output9area.textContent = input9area.value
+        
         
 
     }
@@ -68,7 +74,7 @@ function elevenAm () {
     
 
     function storeInfo11() {
-        localStorage.setItem("UserInput9", input11area.value);
+        localStorage.setItem("UserInput11", input11area.value);
         output11area.textContent = input11area.value
         
 
@@ -96,26 +102,113 @@ function twelvePm () {
     }
 
 }
-function thirteenAm () {
+function thirteenPm () {
     var input1area = document.querySelector('#input1area');
     var output1area = document.querySelector('#input1area');
     var input1Btn = document.querySelector('#button1');
 
-    input9Btn.addEventListener('click', storeInfo9);
+    input1Btn.addEventListener('click', storeInfo1);
 
     
     
-    input9area.value = localStorage.getItem("UserInput9");
-    output9area.textContent = localstorage.getItem("UserInput9");
+    input1area.value = localStorage.getItem("UserInput1");
+    output1area.textContent = localstorage.getItem("UserInput1");
     
 
-    function storeInfo9() {
-        localStorage.setItem("UserInput9", input9area.value);
-        output9area.textContent = input9area.value
+    function storeInfo1() {
+        localStorage.setItem("UserInput1", input1area.value);
+        output1area.textContent = input1area.value
         
 
     }
 
 }
+
+function fourteenPm () {
+    var input2area = document.querySelector('#input2area');
+    var output2area = document.querySelector('#input2area');
+    var input2Btn = document.querySelector('#button2');
+
+    input2Btn.addEventListener('click', storeInfo2);
+
+    
+    
+    input2area.value = localStorage.getItem("UserInput2");
+    output2area.textContent = localstorage.getItem("UserInput2");
+    
+
+    function storeInfo2() {
+        localStorage.setItem("UserInput2", input2area.value);
+        output2area.textContent = input2area.value
+        
+
+    }
+
+}
+function fifteenPm () {
+    var input3area = document.querySelector('#input3area');
+    var output3area = document.querySelector('#input3area');
+    var input3Btn = document.querySelector('#button3');
+
+    input3Btn.addEventListener('click', storeInfo3);
+
+    
+    
+    input3area.value = localStorage.getItem("UserInput3");
+    output3area.textContent = localstorage.getItem("UserInput3");
+    
+
+    function storeInfo3() {
+        localStorage.setItem("UserInput3", input3area.value);
+        output3area.textContent = input3area.value
+        
+
+    }
+
+}
+function sixteenPm () {
+    var input4area = document.querySelector('#input4area');
+    var output4area = document.querySelector('#input4area');
+    var input4Btn = document.querySelector('#button4');
+
+    input4Btn.addEventListener('click', storeInfo4);
+
+    
+    
+    input4area.value = localStorage.getItem("UserInput4");
+    output4area.textContent = localstorage.getItem("UserInput4");
+    
+
+    function storeInfo4() {
+        localStorage.setItem("UserInput4", input4area.value);
+        output4area.textContent = input4area.value
+        
+
+    }
+
+}
+function seventeenPm () {
+    var input5area = document.querySelector('#input5area');
+    var output5area = document.querySelector('#input5area');
+    var input5Btn = document.querySelector('#button5');
+
+    input5Btn.addEventListener('click', storeInfo5);
+
+    
+    
+    input5area.value = localStorage.getItem("UserInput5");
+    output5area.textContent = localstorage.getItem("UserInput5");
+    
+
+    function storeInfo5() {
+        localStorage.setItem("UserInput5", input5area.value);
+        output5area.textContent = input5area.value
+        
+
+    }
+
+}
+
+
 
 
